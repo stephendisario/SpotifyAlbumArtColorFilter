9c3127e7dd010e22800de94d3f6a4a79b58789c7
stephendisario <sdisario@umass.edu>
2021-03-06 10:07:49 -0500

    collage updates

--- src/Containers/Dashboard/Collage/Collage.js
+++ src/Containers/Dashboard/Collage/Collage.js
@@ -1,7 +1,7 @@
 import React, { useEffect,useContext,useState } from 'react'
 import injectWithColor from "../../../util/ColorThief/ColorTheif"
 import {AccessTokenContext} from "../../Dashboard/DashboardRouter"
-import {Image,Layout,Menu,Select,Button} from 'antd'
+import {Image,Layout,Menu,Select,Button,Form,Row,Col} from 'antd'
 import getUsersLikedSongs from "../rest/getUsersLikedSongs"
 import rgbToHsl from "../../../util/rgbToHsl"
 import "../Home/Home.css"
@@ -16,72 +16,101 @@ const Collage = () => {
     const {Content} = Layout;
     const { Option } = Select;
 
-    const menu = (
-        <Menu>
-          <Menu.Item>
-              1st menu item
-          </Menu.Item>
-          <Menu.Item>
-              2nd menu item
-          </Menu.Item>
-          <Menu.Item>
-              3rd menu item
-          </Menu.Item>
-        </Menu>
-      );
-      
+    const [form] = Form.useForm();
 
-    // useEffect(async () => {
-    //     let likedSongs = await getUsersLikedSongs(accessToken)
-    //     let likedSongsWithRgb = await injectWithColor(likedSongs)
-    //     let likedSongsWithHsl = likedSongsWithRgb.map(track => {
-    //         let newTrack = {...track}
-    //         newTrack.color = rgbToHsl(track.color)
-    //         return newTrack
-    //     })
-    //     setAllSongs(likedSongsWithHsl)
-    //     // let sortedByColor = likedSongsWithHsl.sort((track1,track2) => {
-    //     //     return track1.color[0] - track2.color[0]
-    //     // })
-    //     // console.log("1",sortedByColor)
+    const onFinish = (values) => {
+        console.log(values);
+    };
 
-    //     // let newSortedTracks = sortedByColor.filter((e, i, a) => i > 0 ? e.track.album.name !== a[i - 1].track.album.name : true);
+    const onReset = () => {
+        form.resetFields();
+    };
 
-    //     // setCollageSongs(newSortedTracks)
-    // },[])
+    useEffect(async () => {
+        let likedSongs = await getUsersLikedSongs(accessToken)
+        let likedSongsWithRgb = await injectWithColor(likedSongs)
+        let likedSongsWithHsl = likedSongsWithRgb.map(track => {
+            let newTrack = {...track}
+            newTrack.color = rgbToHsl(track.color)
+            return newTrack
+        })
+        setAllSongs(likedSongsWithHsl)
+        let sortedByColor = likedSongsWithHsl.sort((track1,track2) => {
+            return track1.color[0] - track2.color[0]
+        })
+        console.log("1",sortedByColor)
+
+        let newSortedTracks = sortedByColor.filter((e, i, a) => i > 0 ? e.track.album.name !== a[i - 1].track.album.name : true);
+
+        setCollageSongs(newSortedTracks)
+    },[])
 
     useEffect(() => {
         console.log(allSongs)
     },[allSongs])
 
+    //30x30 37px
+    //20x20 54px
+
     return(
         <>
             <Layout className="main-page">
-                <Content className="main-page">
-                <Select allowClear={true} defaultValue="Size" style={{ width: 120 }} >
-                    <Option value="Size"></Option>
-                    <Option value="10">10x10</Option>
-                    <Option value="20">20x20</Option>
-                    <Option value="30">30x30</Option>
-                </Select>
-                <Select defaultValue="Style" style={{ width: 120 }} >
-                    <Option value="color">Color</Option>
-                    <Option value="brightness">Brightness</Option>
-                    <Option value="30">30x30</Option>
-                </Select>
-                <Select defaultValue="Direction" style={{ width: 120 }} >
-                    <Option value="horizontal">Horizontal</Option>
-                    <Option value="vertical">Vertical</Option>
-                    <Option value="diagonal">Diagnonal</Option>
-                </Select>
-                <Select defaultValue="Quality" style={{ width: 120 }} >
-                    <Option value="1">1</Option>
-                    <Option value="2">2</Option>
-                    <Option value="3">3</Option>
-                </Select>
-                <Button>
-                    Clear Filters
-                </Button>
+                <Content className="main-page" style={{paddingTop:"20px"}}>
+                    <Form form={form} layout="inline" onFinish={onFinish}>
+                        <Col span={4} offset={2}>
+                            <Form.Item name="size" rules={[{required: true}]}>
+                                <Select placeholder="Size" allowClear>
+                                    <Option value="10">10x10</Option>
+                                    <Option value="20">20x20</Option>
+                                    <Option value="30">30x30</Option>
+                                </Select>
+                            </Form.Item>
+                        </Col>
+                        <Col span={4}>
+                            <Form.Item  name="sort" rules={[{required: true}]} >
+                                <Select placeholder="Sort By" allowClear>
+                                    <Option value="color">Color</Option>
+                                    <Option value="brightness">Brightness</Option>
+                                </Select>
+                            </Form.Item>
+                        </Col>
+                        <Col span={4}>
+                            <Form.Item  name="direction" rules={[{required: true}]}>
+                                <Select placeholder="Direction" allowClear>
+                                    <Option value="vertical">Vertical</Option>
+                                    <Option value="horizontal">Horizontal</Option>
+                                    <Option value="diagonal">Diagnonal</Option>
+                                </Select>
+                            </Form.Item>
+                        </Col>
+                        <Col span={4}>
+                            <Form.Item  name="vibrance" rules={[{required: true}]} >
+                                <Select placeholder="Vibrance" allowClear>
+                                    <Option value="1">1</Option>
+                                    <Option value="2">2</Option>
+                                    <Option value="3">3</Option>
+                                </Select>
+                            </Form.Item>
+                        </Col>
+                        <Col span={4} style={{textAlign:"end"}}>
+                            <Form.Item>
+                                <Button htmlType="button" onClick={onReset}>Reset</Button> &emsp;
+                                <Button htmlType="submit">Generate</Button>
+                            </Form.Item>
+                        </Col>
+                    </Form>
+                    <Row justify="center" style={{marginTop:"20px"}}>
+                        <Col style={{height:"80vw",width:"80vw"}}>
+                                        {/* Requred for images to not have whitespace underneath them */}
+                            <div style={{fontSize:"0"}}>
+                            {collageSongs && collageSongs.map((track,index) => {
+                                return(
+                                    <Image width={"54px"} height={"54px"} key={index} src={track.track.album.images[2]?.url} preview={false}/>
+                                )
+                            })}
+                            </div>
+                        </Col>
+                    </Row>
                 </Content>
             </Layout>
 
